1290f33d812a9e699ff634c1759773a02b79eed4
darshi <darshi373@gmail.com>
2023-06-07 11:51:42 +0530

    add alias

--- destructure.js
+++ destructure.js
@@ -1,7 +1,7 @@
 let obj1 ={
     id : 123,
-    name : "dj"
-    
+    name : "dj",
+    address : "Ramghat"
 }
 //old way to assign one object to another object
 let obj2 = obj1
@@ -13,3 +13,17 @@ let  {id,name} = obj1
 //the key name and new object variable name must be same
 console.log(id);
 console.log(typeof(id));
+//if we dont want to use let or const within declaration then we can use ()
+let address;
+({address}=obj1)
+console.log(address);
+//old way to add new key value pair
+obj1.skills = ["java","python","javascript","oracle"]
+console.log(obj1);
+//add new variable and default value using destruture
+let {department = "computer science"} = obj1;
+console.log(id,name,address,department);
+console.log(obj1);
+//to add alias to reduce the conflict of variables name
+let {address : permanent_address} = obj1
+console.log(permanent_address);
