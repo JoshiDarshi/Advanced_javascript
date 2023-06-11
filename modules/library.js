74578cfd2dd97d00affb2deab7337fad564e0f53
darshi <darshi373@gmail.com>
2023-06-11 13:47:17 +0530

    module _game

---
+++ modules/library.js
@@ -0,0 +1,12 @@
+export default function(){
+    console.log("I am default function");
+}
+
+// export let message = "Hello advanced java"
+// export function user(name) {
+//     console.log(`Hello, ${name}`);
+// }
+
+// //another way to export the things
+
+// //export {message,user}
---
+++ modules/main.js
@@ -0,0 +1,7 @@
+import {default as darshi} from "./library.js"
+darshi()
+// import { message ,user} from "./library.js";
+// console.log(message);//to display on console
+// document.body.innerHTML = message; //to display on web page
+
+// user("darshi");
